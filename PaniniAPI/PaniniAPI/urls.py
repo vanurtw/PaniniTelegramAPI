3b19485711b6b6de389071c451f8583c1c78c999
vano <vanuartw@mail.ru>
2025-08-25 16:46:56 +0300

    маршрутизация users

--- PaniniAPI/PaniniAPI/urls.py
+++ PaniniAPI/PaniniAPI/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('users.urls')),
+    path('api/v1/', include('users.urls')),
 ]
--- users/urls.py
+++ users/urls.py
@@ -2,5 +2,5 @@ from rest_framework.urls import path
 from .views import TelegramAuthView
 
 urlpatterns = [
-    path('login/', TelegramAuthView.as_view()),
+    path('auth/login/', TelegramAuthView.as_view()),
 ]
